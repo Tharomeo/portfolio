14bba0595edb8386b6bf244a18c94189426aa6bf
= <=>
2023-03-02 20:14:49 -0300

    fix: fixing repeat elements

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <Guides />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Guides from './components/Guides.vue';
+
+export default defineComponent({
+  components: {
+    Guides
+  },
+});
+</script>
+
+<style>
+/* 
+black marmore #040501 || #b69d59 || #848484
+white marmore #f1edea || #e8be5b || #bca990
+*/
+
+*{
+  margin: 0;
+  padding: 0;
+  border: none;
+}
+
+.app {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("@/assets/whitemarmore.jpg");
+  background-size: 100% 100%;
+}
+
+</style>
---
+++ src/components/Guides.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="teste">
+        <h1>testeeee</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    
+    export default defineComponent({
+        name: "GuideComponent"
+    })
+</script>
+
+<style scoped>
+    .teste{
+        background-color: rebeccapurple;
+        width: 30%;
+        height: 100vh;
+    }
+</style>
+
